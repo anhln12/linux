35d1a024ddd508d171a6a8d871075bf2a050990a
Le Anh <anhle0412@gmail.com>
2024-08-14 17:56:11 +0700

    Update ubuntu_fix.sh

--- fixANTT/ubuntu_fix.sh
+++ fixANTT/ubuntu_fix.sh
@@ -1,2 +1,17 @@
 # remove root account from logging to ssh
 sed -i 's/PermitRootLogin yes/PermitRootLogin no/g' /etc/ssh/sshd_config
+
+# Changing SSH port and account lockout policy
+nano /etc/ssh/sshd_config
+Port 222
+MaxAuthTries 5 # MaxAuthTries will lock out IP address if it enters wrong password in more than 5 attempts (you can set this lower or higher).
+
+# Allow port 222
+sudo ufw allow 222
+
+# Timeout Idle Value
+nano /etc/ssh/sshd_config
+ClientAliveInterval 180
+
+
+
